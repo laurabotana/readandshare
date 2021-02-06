8a070c329500769541a6c98c0dc276dcf5ad04ec
Laura <Laura@DESKTOP-FKT9U6K>
2021-02-06 19:56:10 +0100

    Si varios usuarios comparten localización, aparecen juntos en marcador

--- src/main/webapp/resources/js/cercanos.js
+++ src/main/webapp/resources/js/cercanos.js
@@ -33,39 +33,89 @@ $( document ).ready(function() {
 	
 });
 
+function composeTitle(marker) {
+	var title = null;
+	if(Array.isArray(marker) && marker.length > 0) {
+		for(var i = 0; i<marker.length; i++) {
+			if(title === null) {
+				title = '';
+			} else {
+				title = title.concat(', ');
+			}
+			title = title.concat(marker[i].alias);
+		}
+	}
+	return title;
+}
+
+function composeLinks(marker) {
+	var links = null;
+	if(Array.isArray(marker) && marker.length > 0) {
+		for(var i = 0; i<marker.length; i++) {
+			if(links === null) {
+				links = '';
+			} else {
+				links = links.concat('<br />');
+			}
+			links = links.concat('<a href="usuario.html?alias=' + marker[i].alias + '">'+ marker[i].alias +'</a>');
+		}
+	}
+	return links;
+}
+
 function marcarMapa(datos, map, icono) {
 	$.ajax({
 		url: location.protocol + '//nominatim.openstreetmap.org/search?format=json&q='+unescape(encodeURIComponent(datos.localidad + ', ' + datos.provincia)),
 		success: function (data) {
 			if(Array.isArray(data) && data.length > 0) {
+				var title = null;
+				var links = null;
 				var coord = null;
 				for(var i = 0; i<data.length;i++) {
-					var encontrado = false;
+					var pos = null;
 					if(Array.isArray(markers) && markers.length > 0) {
 						for(var j = 0; j<markers.length; j++) {
-							if(data[i].lat === markers[j].lat && data[i].lon === markers[j].lon) {
-								encontrado = true;
-								break;
+							if(Array.isArray(markers[j]) && markers[j].length > 0) {
+								for(var k = 0; k<markers[j].length; k++) {
+									if(data[i].lat === markers[j][k].coord.lat && data[i].lon === markers[j][k].coord.lon) {
+										pos = j;
+										break;
+									}
+								}
+								if(pos !== null) {
+									break;
+								}
 							}
 						}
 					}
-					if(!encontrado) {
-						coord = {
+					var datosUsuario = {
+						alias: datos.alias,
+						coord : {
 							lat: data[i].lat,
 							lon: data[i].lon
-						};
-						markers.push(coord);
-						break;
+						}
 					}
+					coord = datosUsuario.coord;
+					if(pos !== null) {
+						var marker = markers[pos];
+						marker.push(datosUsuario);
+						markers[pos] = marker;
+					} else {
+						var marker = new Array();
+						marker.push(datosUsuario);
+						markers.push(marker);
+					}
+					title = composeTitle(pos !== null ? markers[pos] : markers[markers.length -1]);
+					links = composeLinks(pos !== null ? markers[pos] : markers[markers.length -1]);
 				}
-				if(coord !== null) {
+				if(coord !== null && title !== null && links !== null) {
 					L.marker(
 					[ coord.lat, coord.lon ], {
-						title : datos.alias,
+						title : title,
 						draggable : false,
 						opacity : 1,
 						icon : icono
-					}).bindPopup('<a href="usuario.html?alias=' + datos.alias + '">'+ datos.alias +'</a>').addTo(map);
+					}).bindPopup(links).addTo(map);
 				}
 			}   
 		}
